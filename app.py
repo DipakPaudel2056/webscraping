78c56c985f4e4165deac1ba469d0d366bf30fd81
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-22 21:48:50 +0800

    succesfully pulled 25 data from nike's website

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # let's code the scraper
 import requests
 from bs4 import BeautifulSoup
+# to write in csv file
+import csv
 
 def scrape():
     url = "https://www.nike.com/au/w/mens-shoes-nik1zy7ok"
@@ -10,18 +12,17 @@ def scrape():
     # now i have got the soup let's try to get select only necessary tags from here
     # lets tag a div and 
     allshoes = soup.css.select('.product-card')
-    for shoe in allshoes:
-        shoenametag = shoe.find('div',class_='product-card__title')
-        shoesubtitletag = shoe.find('div',class_='product-card__subtitle')
-        shoepricetag = shoe.find('div',class_='product-price')
-        shoename = shoenametag.text.strip()
-        shoesubtitle = shoesubtitletag.text.strip()
-        shoeprice =shoepricetag.text.strip()
-        
-        print(shoesubtitle)
-        print(shoename)
-        print(shoeprice)
-        print('---')
+    with open('nikeprice.csv','w',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['title','subtitle','price'])
+        for shoe in allshoes:
+            shoenametag = shoe.find('div',class_='product-card__title')
+            shoesubtitletag = shoe.find('div',class_='product-card__subtitle')
+            shoepricetag = shoe.find('div',class_='product-price')
+            shoename = shoenametag.text.strip()
+            shoesubtitle = shoesubtitletag.text.strip()
+            shoeprice =shoepricetag.text.strip()
+            writer.writerow([shoename,shoesubtitle,shoeprice])
     
 if __name__ == '__main__':
     scrape()
