6e6d8ac0a202a65ff338c6425e2cae8ce27b6c4d
dipakpaudel2056 <dipakpaudel2056@gmail.com>
2025-05-23 22:39:25 +0800

    day-2 cleaning

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def scrape():
         prev_height = None
         while True:
             current_height = await page.evaluate("""()=>{window.scrollBy(0,window.innerHeight);return document.body.scrollHeight;}""")
-            if current_height >= 25000:
+            if current_height == prev_height:
                 break
             prev_height = current_height
             await page.wait_for_timeout(4000)
-            print(prev_height,current_height)
             
     async def scrape_infinite_scroll(url):
         async with async_playwright() as p:
@@ -27,41 +26,25 @@ def scrape():
             await scroll_to_bottom(page)
             content = await page.content()
             await browser.close()
-            # response = requests.get(url)
-            # response.raise_for_status()
             soup = BeautifulSoup(content, 'html.parser')
     # # now i have got the soup let's try to get select only necessary tags from here
             allshoes = soup.find_all('div',class_='product-card')
             with open('nikeprice.csv','w',newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(['title','subtitle','price'])
+                writer.writerow(['title','subtitle','price','image'])
                 for shoe in allshoes:
                     shoenametag = shoe.find('div',class_='product-card__title')
                     shoesubtitletag = shoe.find('div',class_='product-card__subtitle')
                     shoepricetag = shoe.find('div',class_='product-price')
+                    shoeimagetag = shoe.find('img')
                     shoename = shoenametag.text.strip()
                     shoesubtitle = shoesubtitletag.text.strip()
                     shoeprice =shoepricetag.text.strip()
-                    writer.writerow([shoename,shoesubtitle,shoeprice])
+                    shoeimgsrc = shoeimagetag['src']
+                    writer.writerow([shoename,shoesubtitle,shoeprice,shoeimgsrc])
                     print([shoename,shoesubtitle,shoeprice])
     asyncio.run(scrape_infinite_scroll(url))
     
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    # # now i have got the soup let's try to get select only necessary tags from here
-    # allshoes = soup.css.select('.product-card')
-    # with open('nikeprice.csv','w',newline='') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(['title','subtitle','price'])
-    #     for shoe in allshoes:
-    #         shoenametag = shoe.find('div',class_='product-card__title')
-    #         shoesubtitletag = shoe.find('div',class_='product-card__subtitle')
-    #         shoepricetag = shoe.find('div',class_='product-price')
-    #         shoename = shoenametag.text.strip()
-    #         shoesubtitle = shoesubtitletag.text.strip()
-    #         shoeprice =shoepricetag.text.strip()
-    #         writer.writerow([shoename,shoesubtitle,shoeprice])
     
 if __name__ == '__main__':
     scrape()
